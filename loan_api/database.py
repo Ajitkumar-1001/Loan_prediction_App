71b15b0885d32cddc334a739e28fbc8f7fc8c39a
Ajitkumar-1001 <sachinajitkumarp@gmail.com>
2025-07-07 18:06:11 +0530

    rectified database

--- loan_api/database.py
+++ loan_api/database.py
@@ -1,10 +1,10 @@
 import sqlite3
-from dotenv import loan_dotenv()
+from dotenv import loan_dotenv
 from pathlib import Path
 import os 
 
 
-var = loan_dotenv()
+var = loan_dotenv
 
 database_path = os.getenv("DATABASE_PATH")
 
