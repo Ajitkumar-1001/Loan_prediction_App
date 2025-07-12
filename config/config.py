2cc91ae9a8c179dccd539fb66e6c48aa402235ef
Ajitkumar-1001 <sachinajitkumarp@gmail.com>
2025-07-12 16:31:18 +0530

    Config files setup

--- config/config.py
+++ config/config.py
@@ -1,4 +1,28 @@
-import dagshub 
+import os 
+from pathlib import Path 
+import yaml 
+from dotenv import load_dotenv
 
 
-dagshub.init(repo_owner='ajitkumarsenthil5', repo_name='Loan_Approval_logs', mlflow=True)
+env_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
+
+def load_yaml(file_name="config.yaml"):
+    yaml_path = Path(__file__).resolve().parent / file_name
+    if not yaml_path.exists():
+        print(f"The yaml file was not found at: {yaml_path}")
+        raise FileNotFoundError(f"{yaml_path} does not exist.")
+    
+    with open(yaml_path, 'r') as conf:
+        return yaml.safe_load(conf)
+
+
+Repo_CONFIG = load_yaml()
+ 
+
+DAGSHUB_TOKEN = os.getenv("DAGSHUB_ACCESS_KEY")
+
+if __name__ == "__main__":
+    print("DAGSHUB TOKEN:", DAGSHUB_TOKEN[:4] + "......" if DAGSHUB_TOKEN else "Not Found")
+    print("Repo Owner:", Repo_CONFIG.get("dagshub", {}).get("repo_owner"))
