a868f69d2c5952605941153255c32158ead3bdda
Ajitkumar-1001 <sachinajitkumarp@gmail.com>
2025-07-16 16:52:11 +0530

    Created predict for loan_approval in FastAPI

--- loan_api/routers/loanapproval.py
+++ loan_api/routers/loanapproval.py
@@ -1,4 +1,8 @@
 from fastapi import APIRouter, HTTPException
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 from models.Loan_approval import LoanApproval
 from joblib import load 
 import numpy as np 
